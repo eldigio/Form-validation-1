4deab96a3c843bff6f6bfcf5dda1295fc244e6db
ElDiGio <samuel.dgv@gmail.com>
2023-01-26 11:50:52 +0100

    fist commit

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 
 // Selectors
-const form = document.querySelector("form");
+const form = document.querySelector('form');
 const email = document.querySelector("input[name='email']");
 const password = document.querySelector("input[name='password']");
 const inputFields = document.querySelectorAll(
-  "form input[required], form select[required]"
+  'form input[required], form select[required]'
 );
-const errorFields = document.querySelectorAll(".invalid-feedback");
-const passwordButton = document.querySelector(".btn-password-toggle");
+const errorFields = document.querySelectorAll('.invalid-feedback');
+const passwordButton = document.querySelector('.btn-password-toggle');
 
 // Funzioni
 
